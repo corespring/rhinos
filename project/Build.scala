16eb2d5cef590f0d5cc9be3ba0c50a463437f485
edeustace <ed.eustace@gmail.com>
2014-01-07 13:06:36 +0000

    fix version

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,6 @@ object Build extends sbt.Build {
   def buildSettings = Defaults.defaultSettings ++ releaseSettings ++ Seq(
     organization := "com.scalapeno",
     scalaVersion := "2.10.0",
-    version := appVersion,
     resolvers ++= Seq(
       "typesafe releases" at "http://repo.typesafe.com/typesafe/releases/",
       "typesafe snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
